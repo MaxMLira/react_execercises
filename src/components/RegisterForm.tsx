7eef69aed62eca9b1381c7d41c6c47a340c45d91
mali1122 <maxwell.lira@netcracker.com>
2023-09-01 15:38:20 -0300

    first implementation register exercice

---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,57 @@
+import {useRef, useState} from "react";
+type User ={
+    firstName: string;
+    lastName: string;
+    email: string;
+}
+export const RegisterForm  = ()=>{
+    const [user, setUser] = useState<User>({firstName: '', lastName: '', email:''})
+    const firsNameRef = useRef('');
+    const lastNameRef = useRef('');
+    const emailNameRef = useRef('');
+    const onSubmit = () =>{
+
+    }
+
+    const register = (newUser:User) => {
+
+    };
+    return (
+        <div className="form-container">
+            <form className="register-form">
+                {/* Uncomment the next line to show the success message */}
+                {/* <div className="success-message">Success! Thank you for registering</div> */}
+                <input
+                    id="first-name"
+                    className="form-field"
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                />
+                {/* Uncomment the next line to show the error message */}
+                {/* <span id="first-name-error">Please enter a first name</span> */}
+                <input
+                    id="last-name"
+                    className="form-field"
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastName"
+                />
+                {/* Uncomment the next line to show the error message */}
+                {/* <span id="last-name-error">Please enter a last name</span> */}
+                <input
+                    id="email"
+                    className="form-field"
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                />
+                {/* Uncomment the next line to show the error message */}
+                {/* <span id="email-error">Please enter an email address</span> */}
+                <button className="form-field" type="submit">
+                    Register
+                </button>
+            </form>
+        </div>
+    );
+}
